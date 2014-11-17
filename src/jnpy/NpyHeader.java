29b4fea2566f3b802b64511a6a78a839883f7d98
mcannamela <m.cannamela@gmail.com>
2014-11-16 22:19:55 -0500

    add new source, make gitignore, remove netbeans stuff from repo

---
+++ src/jnpy/NpyHeader.java
@@ -0,0 +1,37 @@
+package jnpy;
+
+import java.io.UnsupportedEncodingException;
+
+public class NpyHeader {
+    
+    private static final byte majorVersionNumber = (byte) 0x01;
+    private static final byte minorVersionNumber = (byte) 0x00;
+    
+    private static final byte[] getMagicBytes(){
+        byte[] magic = new byte[6];
+        
+        magic[0] = (byte) 0x93;
+        
+        
+        String s = new String("NUMPY");
+        byte[] numpyBytes;
+        try{
+            numpyBytes = s.getBytes("UTF-8");
+        }
+        catch (UnsupportedEncodingException e){
+            throw new RuntimeException("Bad charset, I guess.");
+        }
+        
+        for (int i=1; i<6; i++){
+            magic[i] = numpyBytes[i-1];
+        }
+        
+        
+        return magic;
+    } 
+    
+    
+    
+    
+    
+}
